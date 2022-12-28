128a23aedcd249dcdc9b1cb40b7b28298a08bcea
DEEPAK SINGH BISHT <110534750+Deepak-Singh-Bisht@users.noreply.github.com>
2022-12-28 14:17:56 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3)// we are using nodejs,not browser
+console.log(3
+    +
+    3)// code readability shound be high
+console.log("Deepak")
+
+let name="deepak"
+let age=18
+let islogIn=false
+let state;
+// number=> 2 to power 53
+// bigint
+// string => " "
+//boolean => true/false
+//null => standalone value
+// undefined =>
+//symbol => unique
+//object
+
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
